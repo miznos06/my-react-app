336c4331b8f8a5f3efd5cc663badc5d36881e933
miznos <miznos06@gmail.com>
2026-02-06 22:27:18 +0900

    text changed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hello World</h1>
+      <h1>Hello Vercel</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
