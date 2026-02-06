fcc662dcdc3c60789c41d0865abcb2224665ce86
miznos <miznos06@gmail.com>
2026-02-06 22:54:46 +0900

    change 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Title } from './title.jsx';
+import { Title } from './Title.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
