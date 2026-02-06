778506735dd09dc153c3c38e20a1dab49a025dc7
miznos <miznos06@gmail.com>
2026-02-06 22:52:30 +0900

    4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Title } from './title.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hello Vercel 2 </h1>
+      <Title />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/Title.jsx
@@ -0,0 +1,3 @@
+export const Title = () => {
+    return <h1>Hello Vercel 4</h1>;
+}
